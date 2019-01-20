914bff8fe9be709588e80391041f33c1a09a3783
Igor <i.kostenich@gmail.com>
2019-01-20 12:25:43 +0300

    Refator
    Updated according to Sasha's comments

--- src/test/java/SeleniumTests/BaseTest.java
+++ src/test/java/SeleniumTests/BaseTest.java
@@ -1,19 +1,36 @@
 package SeleniumTests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseTest {
-    WebDriver driver = getDriver("en");
-    public WebDriverWait wait = new WebDriverWait(driver, 30);
+    public static WebDriver driver = getDriver();
+    public WebDriverWait wait;
+    private String userName = "ihar.kastsenich@itechart-group.com";
+    private String password = "qwaszx@1";
+
+    protected User user = new User(userName, password);
+    public static final String url = "http://www.google.com";
+    public static final String locale = "en";
 
 
-    public static WebDriver getDriver(String locale){
+
+    public static WebDriver getDriver(){
         System.setProperty("webdriver.chrome.driver", "C:\\Java Practice\\chromedriver_win32\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--lang=" + locale);
-        return new ChromeDriver(options);
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get(url);
+        return driver;
     }
+
+    public static void waitForElementToBeVisible(By by) {
+        new WebDriverWait(driver, 30).until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
 }
--- src/test/java/SeleniumTests/LogInToGmailTest.java
+++ src/test/java/SeleniumTests/LogInToGmailTest.java
@@ -13,45 +13,44 @@ public class LogInToGmailTest extends BaseTest {
 
     @Test
     public void LogIn() {
-        driver.manage().window().maximize();
-        String username = "ihar.kastsenich@itechart-group.com";
-        String password = "qwaszx@1";
+        String englishLinkText = "English";
+        String signInButtonLocator = "//a[@class=\"gb_Ue gb_Ba gb_Tb\"]";
+        String emailInputFieldLocator = "zHQkBf";
+        String nextButtonLocator = "DL0QTb";
+        String enterPasswordLabelLocator = "u3bW4e";
+        String passwordFieldLocator = "zHQkBf";
+        String userThumbnailLocator = "gbii";
 
-        //Open google.com
-        driver.get("http://www.google.com");
 
-        //Switch language
-        WebElement changeLanguageEnglishLink = driver.findElement(By.linkText("English"));
+        WebElement changeLanguageEnglishLink = driver.findElement(By.linkText(englishLinkText));
         changeLanguageEnglishLink.click();
 
 
         //Click the sign in button
-        WebElement signInButton = driver.findElement(By.xpath("//a[@class=\"gb_Ue gb_Ba gb_Tb\"]"));
+        WebElement signInButton = driver.findElement(By.xpath(signInButtonLocator));
         signInButton.click();
 
         //Enter email address
-        WebElement emailsImput = driver.findElement(By.className("zHQkBf"));
-        emailsImput.sendKeys(username);
+        WebElement emailsImput = driver.findElement(By.className(emailInputFieldLocator));
+        emailsImput.sendKeys(user.getUserName());
 
         //Click the "Next" button
-        WebElement nextButton = driver.findElement(By.className("DL0QTb"));
+        WebElement nextButton = driver.findElement(By.className(nextButtonLocator));
         nextButton.click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("u3bW4e")));
 
         //Enter password
-        WebElement passwordField = driver.findElement(By.className("zHQkBf"));
-
-        passwordField.sendKeys(password);
+        waitForElementToBeVisible(By.className(enterPasswordLabelLocator));
+        WebElement passwordField = driver.findElement(By.className(passwordFieldLocator));
+        passwordField.sendKeys(user.getPassword());
 
         //Click the "Next" button
-        WebElement nextButton2 = driver.findElement(By.className("DL0QTb"));
-        nextButton2.click();
+        nextButton = driver.findElement(By.className(nextButtonLocator));
+        nextButton.click();
 
         //Find the user thumbnail
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("gbii")));
-        WebElement userIcon = driver.findElement(By.className("gbii"));
-        Assert.assertNotNull(userIcon, "test");
+        waitForElementToBeVisible(By.className(userThumbnailLocator));
+        WebElement userIcon = driver.findElement(By.className(userThumbnailLocator));
+        Assert.assertNotNull(userIcon);
 
-        driver.close();
     }
 }
